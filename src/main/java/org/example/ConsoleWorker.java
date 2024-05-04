d849920b394df0aa7f8a199e52f78f24357f50cd
Reholly <eu.ustiantsev@yandex.ru>
2024-05-04 12:59:21 +0500

    done

--- src/main/java/org/example/ConsoleWorker.java
+++ src/main/java/org/example/ConsoleWorker.java
@@ -1,4 +1,29 @@
 package org.example;
 
-public class ConsoleWorker {
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class ConsoleWorker implements CommandLineRunner {
+
+    @Autowired
+    private RabbitTemplate rabbit;
+    @Autowired
+    private Receiver receiver;
+
+    @Override
+    public void run(String... args) throws Exception {
+        var reader = new Scanner(System.in);
+        while (true) {
+            System.out.println("Send message:");
+            var message = reader.nextLine();
+                rabbit.convertAndSend("spring-boot-exchange", "foo.bar.baz", message);
+                receiver.Latch.await(10000, TimeUnit.MILLISECONDS);
+        }
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,49 @@
 package org.example;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.boot.SpringApplication;
+import java.util.UUID;
+
+@SpringBootApplication
 public class Main {
+/*    private String fanoutExchangeName = "spring-boot-exchange";;*/
+    private String queue =  "spring-boot"+ UUID.randomUUID();
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(Main.class, args).close();
+    }
+
+    @Bean
+    protected Queue registerQueue() {
+            return new Queue(queue, false, true, true);
+    }
+
+    @Bean
+    protected FanoutExchange registerExchange() {
+        return new FanoutExchange("spring-boot-exchange");
+    }
+
+    @Bean
+    protected Binding registerBinding(Queue queue, FanoutExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange);
+    }
+
+    @Bean
+    protected MessageListenerAdapter registerAdapter(Receiver receiver) {
+        return new MessageListenerAdapter(receiver, "receive");
+    }
+
+    @Bean
+    protected SimpleMessageListenerContainer registerContainer(ConnectionFactory factory, MessageListenerAdapter adapter) {
+        var cont = new SimpleMessageListenerContainer(factory);
+        cont.setQueueNames(queue);
+        cont.setMessageListener(adapter);
+        return cont;
     }
 }
